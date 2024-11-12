9b1e2e0c02e3fbf34e2ce025e25233cfa2a36d78
Kevin Dean <Kevin.Dean@datadevelopment.com>
2024-11-12 14:30:52 -0500

    Refactored ESLint configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,3 @@
-import tseslint from "typescript-eslint";
-import stylistic from "@stylistic/eslint-plugin";
-import jsdoc from "eslint-plugin-jsdoc";
-import esLintConfigLove from "eslint-config-love";
 import { esLintConfigAIDCToolkit } from "@aidc-toolkit/dev";
 
-export default tseslint.config(
-    ...tseslint.configs.strictTypeChecked,
-    stylistic.configs["recommended-flat"],
-    jsdoc.configs["flat/recommended-typescript"],
-    esLintConfigLove,
-    ...esLintConfigAIDCToolkit
-);
+export default esLintConfigAIDCToolkit;
