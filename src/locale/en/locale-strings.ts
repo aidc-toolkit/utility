75b0d03c963d8214f12932e2560597fc7dd33a54
Kevin Dean <Kevin.Dean@datadevelopment.com>
2025-03-10 09:12:27 -0600

    Added formatting to numbers in locale strings.

--- src/locale/en/locale-strings.ts
+++ src/locale/en/locale-strings.ts
@@ -1,11 +1,11 @@
 export const localeStrings = {
     Transformer: {
-        domainMustBeGreaterThanZero: "Domain {{domain}} must be greater than 0",
-        tweakMustBeGreaterThanOrEqualToZero: "Tweak {{tweak}} must be greater than or equal to 0",
-        valueMustBeGreaterThanOrEqualToZero: "Value {{value}} must be greater than or equal to 0",
-        valueMustBeLessThan: "Value {{value}} must be less than {{domain}}",
-        minimumValueMustBeGreaterThanOrEqualToZero: "Minimum value {{minimumValue}} must be greater than or equal to 0",
-        maximumValueMustBeLessThan: "Maximum value {{maximumValue}} must be less than {{domain}}"
+        domainMustBeGreaterThanZero: "Domain {{domain, number}} must be greater than 0",
+        tweakMustBeGreaterThanOrEqualToZero: "Tweak {{tweak, number}} must be greater than or equal to 0",
+        valueMustBeGreaterThanOrEqualToZero: "Value {{value, number}} must be greater than or equal to 0",
+        valueMustBeLessThan: "Value {{value, number}} must be less than {{domain, number}}",
+        minimumValueMustBeGreaterThanOrEqualToZero: "Minimum value {{minimumValue, number}} must be greater than or equal to 0",
+        maximumValueMustBeLessThan: "Maximum value {{maximumValue, number}} must be less than {{domain, number}}"
     },
     RegExpValidator: {
         stringDoesNotMatchPattern: "String {{s}} does not match pattern"
@@ -14,17 +14,17 @@ export const localeStrings = {
         firstZeroFirstCharacter: "Character set must support zero as first character",
         allNumericAllNumericCharacters: "Character set must support all numeric characters in sequence",
         stringMustNotBeAllNumeric: "String must not be all numeric",
-        lengthMustBeGreaterThanOrEqualTo: "Length {{length}} must be greater than or equal to {{minimumLength}}",
-        lengthMustBeLessThanOrEqualTo: "Length {{length}} must be less than or equal to {{maximumLength}}",
-        lengthMustBeEqualTo: "Length {{length}} must be equal to {{exactLength}}",
-        lengthOfComponentMustBeGreaterThanOrEqualTo: "Length {{length}} of {{component}} must be greater than or equal to {{minimumLength}}",
-        lengthOfComponentMustBeLessThanOrEqualTo: "Length {{length}} of {{component}} must be less than or equal to {{maximumLength}}",
-        lengthOfComponentMustBeEqualTo: "Length {{length}} of {{component}} must be equal to {{exactLength}}",
-        invalidCharacterAtPosition: "Invalid character '{{c}}' at position {{position}}",
-        invalidCharacterAtPositionOfComponent: "Invalid character '{{c}}' at position {{position}} of {{component}}",
-        exclusionNotSupported: "Exclusion value of {{exclusion}} is not supported",
+        lengthMustBeGreaterThanOrEqualTo: "Length {{length, number}} must be greater than or equal to {{minimumLength, number}}",
+        lengthMustBeLessThanOrEqualTo: "Length {{length, number}} must be less than or equal to {{maximumLength, number}}",
+        lengthMustBeEqualTo: "Length {{length, number}} must be equal to {{exactLength, number}}",
+        lengthOfComponentMustBeGreaterThanOrEqualTo: "Length {{length, number}} of {{component}} must be greater than or equal to {{minimumLength, number}}",
+        lengthOfComponentMustBeLessThanOrEqualTo: "Length {{length, number}} of {{component}} must be less than or equal to {{maximumLength, number}}",
+        lengthOfComponentMustBeEqualTo: "Length {{length, number}} of {{component}} must be equal to {{exactLength, number}}",
+        invalidCharacterAtPosition: "Invalid character '{{c}}' at position {{position, number}}",
+        invalidCharacterAtPositionOfComponent: "Invalid character '{{c}}' at position {{position, number}} of {{component}}",
+        exclusionNotSupported: "Exclusion value of {{exclusion, number}} is not supported",
         invalidTweakWithAllNumericExclusion: "Tweak must not be used with all-numeric exclusion",
-        endSequenceValueMustBeLessThanOrEqualTo: "End sequence value (start sequence value + count - 1) must be less than {{domain}}"
+        endSequenceValueMustBeLessThanOrEqualTo: "End sequence value (start sequence value + count - 1) must be less than {{domain, number}}"
     },
     RecordValidator: {
         typeNameKeyNotFound: "{{typeName}} \"{{key}}\" not found"
--- src/locale/fr/locale-strings.ts
+++ src/locale/fr/locale-strings.ts
@@ -1,11 +1,11 @@
 export const localeStrings = {
     Transformer: {
-        domainMustBeGreaterThanZero: "Le domaine {{domain}} doit être supérieur à 0",
-        tweakMustBeGreaterThanOrEqualToZero: "Le réglage {{tweak}} doit être supérieur ou égal à 0",
-        valueMustBeGreaterThanOrEqualToZero: "La valeur {{value}} doit être supérieure ou égale à 0",
-        valueMustBeLessThan: "La valeur {{value}} doit être inférieure à {{domain}}",
-        minimumValueMustBeGreaterThanOrEqualToZero: "La valeur minimale {{minimumValue}} doit être supérieure ou égale à 0",
-        maximumValueMustBeLessThan: "La valeur maximale {{maximumValue}} doit être inférieure à {{domain}}"
+        domainMustBeGreaterThanZero: "Le domaine {{domain, number}} doit être supérieur à 0",
+        tweakMustBeGreaterThanOrEqualToZero: "Le réglage {{tweak, number}} doit être supérieur ou égal à 0",
+        valueMustBeGreaterThanOrEqualToZero: "La valeur {{value, number}} doit être supérieure ou égale à 0",
+        valueMustBeLessThan: "La valeur {{value, number}} doit être inférieure à {{domain, number}}",
+        minimumValueMustBeGreaterThanOrEqualToZero: "La valeur minimale {{minimumValue, number}} doit être supérieure ou égale à 0",
+        maximumValueMustBeLessThan: "La valeur maximale {{maximumValue, number}} doit être inférieure à {{domain, number}}"
     },
     RegExpValidator: {
         stringDoesNotMatchPattern: "La chaîne {{s}} ne correspond pas au modèle"
@@ -14,15 +14,15 @@ export const localeStrings = {
         firstZeroFirstCharacter: "Le jeu de caractères doit prendre en charge zéro comme premier caractère",
         allNumericAllNumericCharacters: "Le jeu de caractères doit prendre en charge tous les caractères numériques en séquence",
         stringMustNotBeAllNumeric: "La chaîne ne doit pas être entièrement numérique",
-        lengthMustBeGreaterThanOrEqualTo: "La longueur {{length}} doit être supérieure ou égale à {{minimumLength}}",
-        lengthMustBeLessThanOrEqualTo: "La longueur {{length}} doit être inférieure ou égale à {{maximumLength}}",
-        lengthMustBeEqualTo: "La longueur {{length}} doit être égale à {{exactLength}}",
-        lengthOfComponentMustBeGreaterThanOrEqualTo: "La longueur {{length}} de {{component}} doit être supérieure ou égale à {{minimumLength}}",
-        lengthOfComponentMustBeLessThanOrEqualTo: "La longueur {{length}} de {{component}} doit être inférieure ou égale à {{maximumLength}}",
-        lengthOfComponentMustBeEqualTo: "La longueur {{length}} de {{component}} doit être égale à {{exactLength}}",
-        invalidCharacterAtPosition: "Caractère non valide '{{c}}' à la position {{position}}",
-        invalidCharacterAtPositionOfComponent: "Caractère non valide '{{c}}' à la position {{position}} de {{component}}",
-        exclusionNotSupported: "La valeur d'exclusion de {{exclusion}} n'est pas prise en charge",
+        lengthMustBeGreaterThanOrEqualTo: "La longueur {{length, number}} doit être supérieure ou égale à {{minimumLength, number}}",
+        lengthMustBeLessThanOrEqualTo: "La longueur {{length, number}} doit être inférieure ou égale à {{maximumLength, number}}",
+        lengthMustBeEqualTo: "La longueur {{length, number}} doit être égale à {{exactLength, number}}",
+        lengthOfComponentMustBeGreaterThanOrEqualTo: "La longueur {{length, number}} de {{component}} doit être supérieure ou égale à {{minimumLength, number}}",
+        lengthOfComponentMustBeLessThanOrEqualTo: "La longueur {{length, number}} de {{component}} doit être inférieure ou égale à {{maximumLength, number}}",
+        lengthOfComponentMustBeEqualTo: "La longueur {{length, number}} de {{component}} doit être égale à {{exactLength, number}}",
+        invalidCharacterAtPosition: "Caractère non valide '{{c}}' à la position {{position, number}}",
+        invalidCharacterAtPositionOfComponent: "Caractère non valide '{{c}}' à la position {{position, number}} de {{component}}",
+        exclusionNotSupported: "La valeur d'exclusion de {{exclusion, number}} n'est pas prise en charge",
         invalidTweakWithAllNumericExclusion: "Le réglage ne doit pas être utilisé avec une exclusion entièrement numérique",
         endSequenceValueMustBeLessThanOrEqualTo: "La valeur de la séquence de fin (valeur de la séquence de début + nombre - 1) doit être inférieure à {{domaine}}"
     },
