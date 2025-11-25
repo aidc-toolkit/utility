bcdd4e04fac0b009420984265472e16823ed432e
Kevin Dean <Kevin.Dean@datadevelopment.com>
2025-11-25 17:27:49 -0500

    Eliminated enum type.

--- test/record.test.ts
+++ test/record.test.ts
@@ -5,37 +5,39 @@ import { i18nUtilityInit, RecordValidator } from "../src";
 await i18nUtilityInit(I18nEnvironment.CLI);
 
 describe("Record validator", () => {
-    enum StringEnum {
-        ValueA = "A",
-        ValueB = "B",
-        ValueC = "C",
-        ValueD = "D"
-    }
+    const StringIndexes = {
+        ValueA: "A",
+        ValueB: "B",
+        ValueC: "C",
+        ValueD: "D"
+    } as const;
 
-    const stringEnumRecord: Record<StringEnum, string> = {
-        [StringEnum.ValueA]: "This is for Value A",
-        [StringEnum.ValueB]: "This is for Value B",
-        [StringEnum.ValueC]: "This is for Value C",
-        [StringEnum.ValueD]: "This is for Value D"
+    type StringIndex = typeof StringIndexes[keyof typeof StringIndexes];
+
+    const stringRecord: Record<StringIndex, string> = {
+        [StringIndexes.ValueA]: "This is for Value A",
+        [StringIndexes.ValueB]: "This is for Value B",
+        [StringIndexes.ValueC]: "This is for Value C",
+        [StringIndexes.ValueD]: "This is for Value D"
     };
 
     test("Validation", () => {
-        const stringEnumRecordValidator = new RecordValidator("String enumeration", stringEnumRecord);
+        const stringRecordValidator = new RecordValidator("String index", stringRecord);
 
         expect(() => {
-            stringEnumRecordValidator.validate("A");
+            stringRecordValidator.validate("A");
         }).not.toThrow(RangeError);
         expect(() => {
-            stringEnumRecordValidator.validate("B");
+            stringRecordValidator.validate("B");
         }).not.toThrow(RangeError);
         expect(() => {
-            stringEnumRecordValidator.validate("C");
+            stringRecordValidator.validate("C");
         }).not.toThrow(RangeError);
         expect(() => {
-            stringEnumRecordValidator.validate("D");
+            stringRecordValidator.validate("D");
         }).not.toThrow(RangeError);
         expect(() => {
-            stringEnumRecordValidator.validate("E");
-        }).toThrow("String enumeration \"E\" not found");
+            stringRecordValidator.validate("E");
+        }).toThrow("String index \"E\" not found");
     });
 });
